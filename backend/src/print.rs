f193affb17aef6bb1fbb8acb63faa09579b20d61
Marlon Kersting <marlon.kersting@iserv.eu>
2025-11-17 00:08:33 +0100

    Added printing struct and simple webcall to print

---
+++ backend/src/print.rs
@@ -0,0 +1,84 @@
+use escpos::errors::PrinterError;
+use escpos::printer::Printer;
+use escpos::ui::line::{LineBuilder, LineStyle};
+use escpos::utils::*;
+use escpos::{driver::*, errors::Result};
+
+pub struct PrintMetaData {
+    pub global_print_count: u32,
+    pub global_message_count: u32,
+    pub message_number: u32,
+    pub message_print_count: u32,
+    pub written_at: String,
+}
+pub struct PrintData {
+    pub print_meta_data: PrintMetaData,
+    pub title: Option<String>,
+    pub author: Option<String>,
+    pub message: String,
+}
+
+pub fn print(print_data: PrintData) -> Result<()> {
+    let driver = UsbDriver::open(0x04b8, 0x0202, None, None)?;
+    let mut printer = Printer::new(driver, Protocol::default(), None);
+
+    // Line
+    let simple_line = LineBuilder::new().style(LineStyle::Simple).build();
+
+    // Name + address
+    printer
+        .init()?
+        .page_code(PageCode::ISO8859_15)?
+        .justify(JustifyMode::LEFT)?
+        .writeln(
+            format!(
+                "format!(Druck Nr. : {}",
+                print_data.print_meta_data.global_print_count
+            )
+            .as_str(),
+        )?
+        .writeln(
+            format!(
+                "Nachricht: {}/{}",
+                print_data.print_meta_data.message_number,
+                print_data.print_meta_data.global_message_count
+            )
+            .as_str(),
+        )?
+        .writeln(
+            format!(
+                "Nachricht gedruckt: {} mal",
+                print_data.print_meta_data.message_print_count
+            )
+            .as_str(),
+        )?
+        .writeln(
+            format!(
+                "Eingetragen am: {} Uhr",
+                print_data.print_meta_data.written_at
+            )
+            .as_str(),
+        )?
+        .cut()?
+        .size(2, 2)?
+        .bold(true)?;
+
+    if let Some(title) = &print_data.title {
+        printer.writeln(title)?;
+    }
+
+    printer.draw_line(simple_line)?.reset_size()?.bold(false)?;
+
+    for line in print_data.message.lines() {
+        printer.writeln(line)?;
+    }
+
+    printer.feed()?;
+    if let Some(author) = &print_data.author {
+        printer.writeln(format!("- {}", author).as_str())?;
+    }
+
+    printer.print_cut()?;
+
+    Result::Ok(())
+}
--- backend/src/web_server.rs
+++ backend/src/web_server.rs
@@ -1,10 +1,29 @@
+use crate::print;
 use axum::{Router, routing::get};
 
 pub async fn run_webserver() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(print_me_some));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn print_me_some() {
+    let printer_data = print::PrintData {
+        print_meta_data: print::PrintMetaData {
+            global_message_count: 10,
+            message_number: 1,
+            global_print_count: 5,
+            message_print_count: 1,
+            written_at: "16.11.2025 23:55".to_string(),
+        },
+        title: Some("Test Titel Nr. 1 Wird sehr sehr lang, was passiert dann wohl!?".to_string()),
+        message: "Diese Nachricht ist \n sehr wichtig!\n\n Jajaja12324566788\n!@#$%^&*(){}-=\\|"
+            .to_string(),
+        author: Some("Marlon".to_string()),
+    };
+
+    print::print(printer_data).expect("Printer fucked");
 }
